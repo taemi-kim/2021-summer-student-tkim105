e34b20d5be3275840be16f96f9b55e67299983b0
Taemi Kim <tkim105@ugradx.cs.jhu.edu>
2021-07-12 14:56:54 -0400

    Done with str_to_lower & bool functions/drafted until L command lines

--- Homework/HW3/phone_db.cpp
+++ Homework/HW3/phone_db.cpp
@@ -23,7 +23,11 @@ typedef struct {
 */
 string str_to_lower(const string &s) {
   // TODO: implement this function and update return value
-  return "";
+  string result = s;
+  for (size_t i = 0; i < s.size(); i++) {
+    result[i] = tolower(s[i]);
+  }
+  return result;
 }
 
 /* Compare two Name structures, ignoring capitalization.
@@ -33,6 +37,16 @@ string str_to_lower(const string &s) {
 */
 bool operator<(const Name &left, const Name &right) {
   // TODO: implement this function and update return value
+  string left_lastname = str_to_lower(left.lastname);
+  string left_firstname = str_to_lower(left.firstname);
+  string right_lastname = str_to_lower(right.lastname);
+  string right_firstname = str_to_lower(left.firstname);
+  if (left_lastname == right_lastname) { //same lastnames
+    return left_firstname < right_firstname;
+  }
+  else {
+    return left_lastname < right_lastname;
+  }
   return false;
 }
 
@@ -40,9 +54,60 @@ bool operator<(const Name &left, const Name &right) {
 // a map before the comparison function above, only after it.
 
 // TODO: define the other data types used to represent the phone list
+typedef vector<string> PhoneNumberCollection;
+map<Name, PhoneNumberCollection> phone_db;
 
-int main() {
+int main(/*argc, **argv*/) { //added argc, argv is this correct? 
   // TODO: implement
+  cout << "Info: " << "Welcome to the Phone Database!" << endl; //info messages
+  cout << "Info: " << "Please enter a command" << endl;
+  string contact;
+  while(getline(cin,contact)) {
+      ss //extract 
+      if (contact[0] == 'C') { //creates contact
+	Name temp;
+	temp.lastname = contact[1];
+	temp.firstname = contact[2];
+	for (map<Name, PhoneNumberCollection>::iterator it = phone_db.begin(); it != phone_db.end(); ++it) {
+	  if (it->first == temp) {
+	    cout << "Error: " << "Contact already exists" << endl;
+	  }
+	  else {
+	    phone_db[temp].push_back();
+	    cout << "Result: " << "Contact created" << endl;
+	  }
+	}
+	
+      }
+      else if (contact[0] == 'D') { //deletes a contact
+	Name temp;
+        temp.lastname =	contact[1];
+        temp.firstname = contact[2];
+	for (map<Name, PhoneNumberCollection>::iterator it = phone_db.begin(); it != phone_db.end(); ++it) {
+          if (it->first == temp) {
+	    phone_db.erase (it);
+            cout << "Result: " << "Contact deleted" << endl;
+          }
+          else {
+            phone_db[temp].push_back();
+            cout << "Error: " << "Contact not found" << endl;
+	  }
+        }
 
+      }
+      else if (contact[0] == 'L') { //list contact names
+	sort(phone_db.begin(), phone_db.end(), operator<);
+	for (map<Name, PhoneNumberCollection>::iterator it = phone_db.begin(); it != phone_db.end(); ++it) {
+	  cout << "Result: " << it->first.lastname << ',' << it->first.firstname << endl;
+	}
+      }
+      else if (contact[0] == 'P') { //list phone #s for a contact
+	
+
+      }
+    }
+ 
+  cout << "Result: " << endl; //result messages
+  cout << "Error: " << endl; //error messages
   return 0;
 }
